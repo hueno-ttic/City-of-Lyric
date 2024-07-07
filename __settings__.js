f170edff864e8bd1cd43a12069f8f8653eca544e
hueno-ttic <tsukasa.hiroshi@gmail.com>
2024-07-08 06:30:35 +0900

    test

---
+++ __settings__.js
@@ -0,0 +1,24 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2030172.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 187384521, 187384522, 187384524, 187384525, 187382838, 187384546, 187384542, 187384548, 187382747, 187388385, 187384565, 187382839, 187384569, 187384570, 187384571, 187384572, 187384573, 187384574, 187384575, 187382794, 187382751, 187384576, 187382754, 187384577, 187384578, 187384579, 187384656, 187388378, 187382743, 187382799, 187384895, 187382778, 187382831, 187382835, 187388380, 187382757, 187388382, 187388374, 187388358, 187388366, 187388359, 187388390, 187414951, 187440518, 187460600, 187463103, 187483086, 187495280 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/187404032/1/draco.wasm.js', 'wasmUrl' : 'files/assets/187404033/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/187404031/1/draco.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/187436602/1/basis.wasm.js', 'wasmUrl' : 'files/assets/187436600/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/187436601/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/187382843/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/187382844/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/187382842/1/ammo.js', 'preload' : true},
+];
